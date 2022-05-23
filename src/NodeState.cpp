1c51fd22bd1f31eea3f477ef1ca6a544f553a675
Pedro Callado <pedrocallado@gmail.com>
2022-05-23 16:55:03 -0300

    removes assertions and unnecessary code

--- src/NodeState.cpp
+++ src/NodeState.cpp
@@ -143,7 +143,6 @@ insertPort(const PortType& portType,
            const size_t index)
 {
   auto& ports = getEntries(portType);
-  assert(index <= ports.size());
   ports.emplace(std::next(ports.begin(), index));
   updateConnectionIndices(portType, index + 1);
 }
@@ -154,14 +153,11 @@ erasePort(const PortType portType,
           const size_t index)
 {
   auto& ports = getEntries(portType);
-  assert(index < ports.size());
-
   auto erased_port_map_it = std::next(ports.begin(), index);
 
   // erases port connections
   for (auto& entry : *erased_port_map_it)
   {
-    assert(int(index) == entry.second->getPortIndex(portType));
     eraseConnection(portType, index, entry.first);
   }
 
@@ -169,15 +165,6 @@ erasePort(const PortType portType,
   ports.erase(erased_port_map_it);
 
   // reassigns subsequent ports
-  for (auto& port : ports) {
-    for (auto& conn_ptr : port) {
-      auto conn_idx = conn_ptr.second->getPortIndex(portType);
-      if (conn_idx > static_cast<int>(index)) {
-          conn_ptr.second->setPortIndex(portType, conn_idx - 1);
-      }
-    }
-  }
-
   updateConnectionIndices(portType, index);
 }
 
