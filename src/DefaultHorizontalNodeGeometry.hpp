ea90e2da1ffe511dacd147cf8a6e25fae981f5ca
Martinelle Santos <martinelle17@gmail.com>
2024-01-03 16:11:26 -0300

    Continue previous merge

--- src/DefaultHorizontalNodeGeometry.hpp
+++ src/DefaultHorizontalNodeGeometry.hpp
@@ -4,28 +4,25 @@
 
 #include <QtGui/QFontMetrics>
 
-
-namespace QtNodes
-{
+namespace QtNodes {
 
 class AbstractGraphModel;
 class BasicGraphicsScene;
 
+class NODE_EDITOR_PUBLIC DefaultHorizontalNodeGeometry
+    : public AbstractNodeGeometry {
+ public:
+  DefaultHorizontalNodeGeometry(AbstractGraphModel& graphModel);
 
-class NODE_EDITOR_PUBLIC DefaultHorizontalNodeGeometry : public AbstractNodeGeometry
-{
-public:
-  DefaultHorizontalNodeGeometry(AbstractGraphModel & graphModel);
-
-public:
+ public:
   QRectF boundingRect(NodeId const nodeId) const override;
 
   QSize size(NodeId const nodeId) const override;
 
   void recomputeSize(NodeId const nodeId) const override;
 
-  QPointF portPosition(NodeId const    nodeId,
-                       PortType const  portType,
+  QPointF portPosition(NodeId const nodeId,
+                       PortType const portType,
                        PortIndex const index) const override;
 
   QPointF portTextPosition(NodeId const nodeId,
@@ -38,19 +35,43 @@ public:
   QPointF widgetPosition(NodeId const nodeId) const override;
 
   QRect resizeHandleRect(NodeId const nodeId) const override;
-private:
 
-  QRectF
-  portTextRect(NodeId const   nodeId,
-               PortType const portType,
-               PortIndex const portIndex) const;
+  /**
+   * @brief Updates the space reserved for the status icon based on the
+   * model's current status.
+   */
+  void updateStatusIconSize() const;
+
+  /**
+   * @brief Returns the size (width and height) of the icon that indicates
+   * the node's current processing status.
+   */
+  QSize statusIconSize() const;
+
+  /**
+   * @brief Returns the dimensions of the icon that indicates the node's
+   * current processing status.
+   */
+  QRect statusIconRect() const;
+
+  /**
+   * @brief Returns the icon associated with the model's current processing
+   * status.
+   */
+  const QIcon& processingStatusIcon() const;
+
+ private:
+  QRectF portTextRect(NodeId const nodeId,
+                      PortType const portType,
+                      PortIndex const portIndex) const;
 
   /// Finds max number of ports and multiplies by (a port height + interval)
   unsigned int maxVerticalPortsExtent(NodeId const nodeId) const;
 
-  unsigned int maxPortsTextAdvance(NodeId const   nodeId,
+  unsigned int maxPortsTextAdvance(NodeId const nodeId,
                                    PortType const portType) const;
-private:
+
+ private:
   // Some variables are mutable because we need to change drawing
   // metrics corresponding to fontMetrics but this doesn't change
   // constness of the Node.
@@ -59,6 +80,32 @@ private:
   unsigned int _portSpasing;
   mutable QFontMetrics _fontMetrics;
   mutable QFontMetrics _boldFontMetrics;
+  mutable QSize _statusIconSize;
+
+  /**
+   * @brief Flag indicating whether the processing status icon should be part
+   * of the node's geometry.
+   */
+  mutable bool _statusIconActive;
+
+  //  std::unique_ptr<NodeDataModel> const& _dataModel;
+
+  /**
+   * @brief Processing status icons
+   */
+  const QIcon _statusUpdated{"://status_icons/updated.svg"};
+  const QIcon _statusProcessing{"://status_icons/processing.svg"};
+  const QIcon _statusPending{"://status_icons/pending.svg"};
+  const QIcon _statusInvalid{"://status_icons/failed.svg"};
+  const QIcon _statusEmpty{"://status_icons/empty.svg"};
+  const QIcon _statusPartial{"://status_icons/partial.svg"};
+
+  unsigned int _entryWidth;  // TODO: where was is used?
+
+  unsigned int _nSources;  // TODO: where was it used?
+  unsigned int _nSinks;
+
+  QPointF _draggingPos;
 };
 
-}
+}  // namespace QtNodes
